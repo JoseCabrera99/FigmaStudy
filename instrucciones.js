198c8568ace7192655765f31e2c989c57ee7ea42
DiegoMesa0 <diegoa.mesav@gmail.com>
2023-02-16 10:45:20 -0500

    Anexo de instrucciones.js al track

---
+++ instrucciones.js
@@ -0,0 +1,18 @@
+const submenu = document.querySelector('.opciones');
+const hiddenmain = document.querySelector('.main-container');
+const hiddenmenu = document.querySelector('.menu');
+const backhome = document.querySelector('.back');
+
+submenu.addEventListener('click', () =>{
+    hiddenmain.classList.toggle('hidden');
+    hiddenmenu.classList.toggle('hidden');
+})
+
+backhome.addEventListener('click', () =>{
+    hiddenmenu.classList.toggle('hidden');
+    hiddenmain.classList.toggle('hidden');
+})
+
+backabout.addEventListener('click', () =>{
+    alert("Hola");
+})
